ee8c2c62d9cd749e98c8f644e84cfdf7fb8ec3f6
yuebei <yuebei58@gmail.com>
2018-06-20 16:05:52 +0800

    Creating mixins.py ,config crispy template pack to bootstrap4

--- django/config/settings.py
+++ django/config/settings.py
@@ -130,3 +130,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# crispy
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+
+
+
+
+
+
+
+
+
+
+
