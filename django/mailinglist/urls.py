f838e832cd83bf6ff516c07e0087b49e339d6167
yuebei <yuebei58@gmail.com>
2018-06-20 17:39:33 +0800

    Create MailingListView and CreateMailingListView, add template page in templates, add path in urls.py

--- django/mailinglist/urls.py
+++ django/mailinglist/urls.py
@@ -5,4 +5,10 @@ from mailinglist import views
 app_name = 'mailinglist'
 
 urlpatterns = [
+    path('',
+         views.MailingListListView.as_view(),
+         name='mailinglist_list'),
+    path('new',
+         views.CreateMailingListView.as_view(),
+         name='create_mailinglist'),
 ]
--- django/mailinglist/views.py
+++ django/mailinglist/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from mailinglist.models import MailingList
+from mailinglist.forms import MailingListForm
+
+
+class MailingListListView(LoginRequiredMixin, ListView):
+
+    def get_queryset(self):
+        return MailingList.objects.filter(owner=self.request.user)
+
+
+class CreateMailingListView(LoginRequiredMixin, CreateView):
+    form_class = MailingListForm
+    template_name = 'mailinglist/mailinglist_form.html'
+
+    def get_initial(self):
+        return {
+            'owner':self.request.user.id,
+        }
