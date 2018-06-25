962f1344b77d5cb76e99b084285a1cb5c37a6fd5
yuebei <yuebei58@gmail.com>
2018-06-25 18:52:13 +0800

    add sending emails to subscribers

--- django/mailinglist/tasks.py
+++ django/mailinglist/tasks.py
@@ -15,3 +15,12 @@ def build_subscriber_messages_for_message(message_id):
     from mailinglist.models import Message, SubscriberMessage
     message = Message.objects.get(id=message_id)
     SubscriberMessage.objects.create_from_message(message)
+
+
+@shared_task
+def send_subscriber_message(subscriber_message_id):
+    from mailinglist.models import SubscriberMessage
+    subscriber_message = SubscriberMessage.objects.get(
+        id=subscriber_message_id)
+    emails.send_subscriber_message(subscriber_message)
+    
